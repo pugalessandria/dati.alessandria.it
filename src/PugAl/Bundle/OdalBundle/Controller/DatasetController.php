d31cbc2b863cbee08a3582986a8c8f34dfd4097c
Luca Lusso <llusso@wellnet.it>
2013-12-28 16:03:27 +0100

    rifinita pagina dettaglio

--- src/PugAl/Bundle/OdalBundle/Controller/DatasetController.php
+++ src/PugAl/Bundle/OdalBundle/Controller/DatasetController.php
@@ -5,21 +5,19 @@ namespace PugAl\Bundle\OdalBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Response;
 
 class DatasetController extends Controller {
 
   /**
    * @Route("/datasets", name="datasets")
-   * @Template()
    */
   public function datasetsAction(Request $request) {
     $datasets = $this->get('pug_al_odal.ApiClient')->listAll();
 
     $content = $this->renderView(
       'PugAlOdalBundle:Dataset:datasets.html.twig',
-      array('title' => 'Datasets', 'datasets' => $datasets,)
+      array('title' => 'Datasets', 'datasets' => $datasets)
     );
 
     $response = new Response($content);
@@ -30,16 +28,19 @@ class DatasetController extends Controller {
 
   /**
    * @Route("/dataset/{id}", name="dataset")
-   * @Template()
    */
   public function datasetAction(Request $request, $id) {
     $dataset = $this->get('pug_al_odal.ApiClient')->getDataset($id);
-    var_dump($dataset);
 
-    return array(
-      'title' => $dataset['title'],
-      'dataset' => $dataset,
+    $content = $this->renderView(
+      'PugAlOdalBundle:Dataset:dataset.html.twig',
+      array('title' => $dataset['title'], 'dataset' => $dataset)
     );
+
+    $response = new Response($content);
+    $response->setMaxAge(600);
+
+    return $response;
   }
 
 }
